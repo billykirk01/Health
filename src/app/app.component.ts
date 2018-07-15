b7d02b69c35768e5b9d93d5391de8b6fb204cad6
wkirk01 <30280510+wkirk01@users.noreply.github.com>
2018-07-15 15:58:02 -0500

    Minor updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -125,42 +125,6 @@ export class AppComponent {
         })
       })
 
-      // this.chartTwo = new Chart(this.canvasTwo.nativeElement.getContext('2d'), {
-      //   type: 'bar',
-      //   data: {
-      //     labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-      //     datasets: [{
-      //       label: '# of Votes',
-      //       data: [12, 19, 3, 5, 2, 3],
-      //       backgroundColor: [
-      //         'rgba(255, 99, 132, 0.2)',
-      //         'rgba(54, 162, 235, 0.2)',
-      //         'rgba(255, 206, 86, 0.2)',
-      //         'rgba(75, 192, 192, 0.2)',
-      //         'rgba(153, 102, 255, 0.2)',
-      //         'rgba(255, 159, 64, 0.2)'
-      //       ],
-      //       borderColor: [
-      //         'rgba(255,99,132,1)',
-      //         'rgba(54, 162, 235, 1)',
-      //         'rgba(255, 206, 86, 1)',
-      //         'rgba(75, 192, 192, 1)',
-      //         'rgba(153, 102, 255, 1)',
-      //         'rgba(255, 159, 64, 1)'
-      //       ],
-      //       borderWidth: 1
-      //     }]
-      //   },
-      //   options: {
-      //     scales: {
-      //       yAxes: [{
-      //         ticks: {
-      //           beginAtZero: true
-      //         }
-      //       }]
-      //     }
-      //   }
-      // });
     })
 
     this.days = this.db.collection('nutrition', ref => ref.orderBy('date', 'desc').limit(5)).valueChanges()
@@ -168,13 +132,16 @@ export class AppComponent {
   }
 
   decrementCurrentDay(){
+    
     if (this.currentDayIndex < 4){
       this.currentDayIndex = this.currentDayIndex + 1
+      console.log(this.currentDayIndex)
       this.testIndex.next(this.currentDayIndex)
     }
   }
 
   incrementCurrentDay(){
+    console.log(this.currentDayIndex)
     if (this.currentDayIndex > 0){
       this.currentDayIndex = this.currentDayIndex - 1
       this.testIndex.next(this.currentDayIndex)
