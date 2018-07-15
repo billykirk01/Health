4ca185d627ba51b287326d6684247047fa7d78b6
wkirk01 <30280510+wkirk01@users.noreply.github.com>
2018-07-15 14:57:01 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Health
+
+This project is equiped with the [Angular Material](https://material.angular.io), [UIkit](https://getuikit.com), and [Semantic UI](https://https://semantic-ui.com/) ready to go out of the box.
+
+## Get Started
+
+Run `git clone https://github.com/wkirk01/AngularStarterProject` at the terminal.
+
+Inside of the newly created directory run `npm install`.
+
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`.
---
+++ src/app/app.component.ts
@@ -0,0 +1,191 @@
+import { Component, ViewChild, ElementRef, } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Chart } from 'chart.js';
+import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable, Subject } from 'rxjs';
+
+import { last } from 'rxjs/operators';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  constructor(private db: AngularFirestore, private breakpointObserver: BreakpointObserver) { }
+
+  @ViewChild('canvasOne') canvasOne: ElementRef;
+  chartOne = [];
+
+  @ViewChild('canvasTwo') canvasTwo: ElementRef;
+  chartTwo = [];
+
+  onMobile: boolean
+
+  days: Observable<any[]>;
+
+  private recentDoc: AngularFirestoreDocument<any>;
+  recent: Observable<any>;
+
+  currentDay: any;
+  currentDayIndex: number = 0;
+
+  testIndex: Subject<any>
+
+  ngOnInit(): void {
+
+    this.breakpointObserver.observe([
+      Breakpoints.HandsetLandscape,
+      Breakpoints.HandsetPortrait
+    ]).subscribe(result => {
+      if (result.matches) {
+        this.onMobile = true
+      } else {
+        this.onMobile = false
+      }
+    });
+
+    this.recentDoc = this.db.doc<any>('recent/totals');
+    this.recent = this.recentDoc.valueChanges();
+    this.recent.subscribe(ary => {
+      this.chartOne = new Chart(this.canvasOne.nativeElement.getContext('2d'), {
+        type: 'bar',
+        data: {
+          labels: ary.dates,
+          datasets: [{
+            label: 'Calories',
+            backgroundColor: 'rgba(153, 102, 255, 0.2)',
+            borderColor: 'rgba(153, 102, 255, 1)',
+            borderWidth: 1,
+            stack: 'Stack 0',
+            yAxisID: 'y-axis-1',
+            data: ary.calories
+          }, {
+            label: 'Carbohydrates',
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1,
+            stack: 'Stack 1',
+            yAxisID: 'y-axis-2',
+            data: ary.carbohydrates
+          }, {
+            label: 'Protein',
+            backgroundColor: 'rgba(88, 214, 141, 0.2)',
+            borderColor: 'rgba(88, 214, 141, 1)',
+            borderWidth: 1,
+            stack: 'Stack 1',
+            yAxisID: 'y-axis-2',
+            data: ary.protein
+          },
+          {
+            label: 'Fat',
+            backgroundColor: 'rgba(255, 206, 86, 0.2)',
+            borderColor: 'rgba(255, 206, 86, 1)',
+            borderWidth: 1,
+            stack: 'Stack 1',
+            yAxisID: 'y-axis-2',
+            data: ary.fat
+          }
+        ]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              id: 'y-axis-1',
+							display: true,
+							position: 'left',
+              beginAtZero: false,
+              suggestedMin: 1700,
+              gridLines: {
+                display: false
+              }
+						}, {
+              id: 'y-axis-2',
+              display: true,
+              position: 'right',
+              gridLines: {
+                display: false
+              }
+						}]
+          },
+          legend: {
+            display: false
+          }
+        }
+      });
+
+      this.testIndex = new Subject()
+
+      this.days.subscribe(ary =>{
+        this.currentDay = ary[0]
+        this.testIndex.subscribe(val => {
+          this.currentDay = ary[val]
+        })
+      })
+
+      // this.chartTwo = new Chart(this.canvasTwo.nativeElement.getContext('2d'), {
+      //   type: 'bar',
+      //   data: {
+      //     labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      //     datasets: [{
+      //       label: '# of Votes',
+      //       data: [12, 19, 3, 5, 2, 3],
+      //       backgroundColor: [
+      //         'rgba(255, 99, 132, 0.2)',
+      //         'rgba(54, 162, 235, 0.2)',
+      //         'rgba(255, 206, 86, 0.2)',
+      //         'rgba(75, 192, 192, 0.2)',
+      //         'rgba(153, 102, 255, 0.2)',
+      //         'rgba(255, 159, 64, 0.2)'
+      //       ],
+      //       borderColor: [
+      //         'rgba(255,99,132,1)',
+      //         'rgba(54, 162, 235, 1)',
+      //         'rgba(255, 206, 86, 1)',
+      //         'rgba(75, 192, 192, 1)',
+      //         'rgba(153, 102, 255, 1)',
+      //         'rgba(255, 159, 64, 1)'
+      //       ],
+      //       borderWidth: 1
+      //     }]
+      //   },
+      //   options: {
+      //     scales: {
+      //       yAxes: [{
+      //         ticks: {
+      //           beginAtZero: true
+      //         }
+      //       }]
+      //     }
+      //   }
+      // });
+    })
+
+    this.days = this.db.collection('nutrition', ref => ref.orderBy('date', 'desc').limit(5)).valueChanges()
+
+  }
+
+  decrementCurrentDay(){
+    if (this.currentDayIndex < 4){
+      this.currentDayIndex = this.currentDayIndex + 1
+      this.testIndex.next(this.currentDayIndex)
+    }
+  }
+
+  incrementCurrentDay(){
+    if (this.currentDayIndex > 0){
+      this.currentDayIndex = this.currentDayIndex - 1
+      this.testIndex.next(this.currentDayIndex)
+    }
+  }
+}
+
+export interface Day {
+  date: string;
+}
+
+export interface lastSeven {
+  calories: [number]
+}
